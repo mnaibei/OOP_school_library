bd11b6d039cfa33efbdffe48e9f2bb820de91d71
mnaibei <muchajulius@gmail.com>
2023-06-20 18:09:45 +0300

    Setup associations

---
+++ book.rb
@@ -0,0 +1,9 @@
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    # @title and @author instance variables
+    @title = title
+    @author = author
+  end
+end
---
+++ classroom.rb
@@ -0,0 +1,8 @@
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = [] # Initialize an empty array to store the students associated with the classroom
+  end
+end
