21049fe38f4f317aebd83aec8070a8e4a00c44b8
mnaibei <muchajulius@gmail.com>
2023-06-29 11:36:04 +0300

    Making tests pass

---
+++ tests/TDD/solver.rb
@@ -0,0 +1,35 @@
+class Solver
+
+    def factorial(number)
+        if number<= 1
+            1
+          else
+            number * factorial( number - 1 )
+          end
+    end
+    
+    def reverse(string)
+        string.reverse
+    end
+    
+    def fizzbuzz(number)
+        return 'fizzbuzz' if number % 15 == 0
+        return 'fizz' if number % 3 == 0
+        return 'buzz' if number % 5 == 0
+        number.to_s
+    end
+    # def factorial(number)
+    #     (1..number).inject(:*) || 1
+    # end
+    
+    # def reverse(string)
+    #     string.reverse
+    # end
+    
+    # def fizzbuzz(number)
+    #     return 'fizzbuzz' if number % 15 == 0
+    #     return 'fizz' if number % 3 == 0
+    #     return 'buzz' if number % 5 == 0
+    #     number.to_s
+    # end
+end
